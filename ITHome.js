a3767a4bf670ccdb0aeaba7b4fc6c9d649e18d8d
Jeff0Z <30599793+Jeff0Z@users.noreply.github.com>
2023-01-05 15:14:57 +0800

    mk file

---
+++ ITHome.js
@@ -0,0 +1,9 @@
+function indexAD(obj) {
+  let data = obj("data");
+  let lsit = data["list"];
+  data["list"] = lsit.filter(item => item.smallTags[0].tagType != 2);
+  return obj;
+}
+
+let obj = JSON.parse($response.body);
+$done({ body: JSON.stringify(indexAD(obj)) });
